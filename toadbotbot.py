7168cbf61efd5029f67219e3a8003f0fed03a4a1
QwertyQwertovich <76122816+QwertyQwertovich@users.noreply.github.com>
2021-03-01 20:34:25 +0300

    Add files via upload

---
+++ toadbotbot.py
@@ -0,0 +1,53 @@
+import sys
+sys.path.append("/home/gametopsss/.local/lib/python3.7/site-packages")
+import vk_api
+import time
+from random import randint
+st=0
+numberr=0
+conv = 1
+conv2 = 1
+conv = 2000000000 + conv
+conv2 = 2000000000 + conv2
+id_to_duel = "id0"
+vk = vk_api.VkApi(token="First account")
+vk2 = vk_api.VkApi(token="Second account")
+while True:
+    vk.method("messages.send",{"peer_id":conv,"random_id":randint(0,1000),"message":"@toadbot Завершить работу"})
+    time.sleep(5)
+    vk2.method("messages.send",{"peer_id":conv2,"random_id":randint(0,1000),"message":"@toadbot Завершить работу"})
+    time.sleep(5)
+    vk.method("messages.send",{"peer_id":conv,"random_id":randint(0,1000),"message":"Дуэль "+id_to_duel})
+    time.sleep(5)
+    vk2.method("messages.send",{"peer_id":conv2,"random_id":randint(0,1000),"message":"Дуэль принять"})
+    time.sleep(5)
+    vk.method("messages.send",{"peer_id":conv,"random_id":randint(0,1000),"message":"Дуэль старт"})
+    time.sleep(30)
+    vk.method("messages.send",{"peer_id":conv,"random_id":randint(0,1000),"message":"Покормить жабу"})
+    time.sleep(5)
+    vk2.method("messages.send",{"peer_id":conv2,"random_id":randint(0,1000),"message":"Покормить жабу"})
+    time.sleep(5)
+    vk.method("messages.send",{"peer_id":conv,"random_id":randint(0,1000),"message":"Дуэль "+ id_to_duel})
+    time.sleep(5)
+    vk2.method("messages.send",{"peer_id":conv2,"random_id":randint(0,1000),"message":"Дуэль принять"})
+    time.sleep(5)
+    vk.method("messages.send",{"peer_id":conv,"random_id":randint(0,1000),"message":"Дуэль старт"})
+    time.sleep(30)
+    vk.method("messages.send", {"peer_id": conv, "random_id": randint(0, 1000), "message": "реанимировать жабу"})
+    time.sleep(5)
+    vk2.method("messages.send", {"peer_id": conv2, "random_id": randint(0, 1000), "message": "реанимировать жабу"})
+    time.sleep(5)
+    vk.method("messages.send", {"peer_id": conv, "random_id": randint(0, 1000), "message": "Дуэль " + id_to_duel})
+    time.sleep(5)
+    vk2.method("messages.send", {"peer_id": conv2, "random_id": randint(0, 1000), "message": "Дуэль принять"})
+    time.sleep(5)
+    vk.method("messages.send", {"peer_id": conv, "random_id": randint(0, 1000), "message": "Дуэль старт"})
+    time.sleep(30)
+    vk.method("messages.send",{"peer_id":conv,"random_id":randint(0,1000),"message":"@toadbot Работа крупье"})
+    time.sleep(5)
+    vk2.method("messages.send",{"peer_id":conv2,"random_id":randint(0,1000),"message":"@toadbot Работа крупье"})
+    time.sleep(5)
+    vk.method("messages.send",{"peer_id":conv,"random_id":randint(0,1000),"message":"@toadbot Поход в столовую"})
+    time.sleep(5)
+    vk2.method("messages.send",{"peer_id":conv2,"random_id":randint(0,1000),"message":"@toadbot Поход в столовую"})
+    time.sleep(2*60*59)
